f6b9e119d103edd12920c05813264c0289c30f8b
parrot-xxiv <30864839+parrot-xxiv@users.noreply.github.com>
2025-04-05 12:03:18 +0800

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,46 @@
+
+const CACHE_NAME = 'quiz-app-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/images/icon-192x192.png',
+  '/images/icon-512x512.png'
+];
+
+// Install the service worker and cache the essential files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Serve cached files when offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((cachedResponse) => {
+        // Return cached response if available, or fetch from network
+        return cachedResponse || fetch(event.request);
+      })
+  );
+});
+
+// Update the service worker
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
